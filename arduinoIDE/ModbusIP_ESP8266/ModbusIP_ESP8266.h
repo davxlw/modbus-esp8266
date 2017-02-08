09aa6bcda9f114b7d1d38b3ef5127668ae48c49d
davxlw <davxlw@free.fr>
2017-02-08 13:39:52 +0100

    configClient() method declaration

--- arduinoIDE/ModbusIP_ESP8266/ModbusIP_ESP8266.h
+++ arduinoIDE/ModbusIP_ESP8266/ModbusIP_ESP8266.h
@@ -1,7 +1,7 @@
 /*
-    ModbusIP_ESP8266.h - Header for Modbus IP ESP8266 Library
-    Copyright (C) 2015 André Sarmento Barbosa
-*/
+ ModbusIP_ESP8266.h - Header for Modbus IP ESP8266 Library
+ Copyright (C) 2015 Andrï¿½ Sarmento Barbosa
+ */
 #include <Modbus.h>
 #include <ESP8266WiFi.h>
 
@@ -12,14 +12,14 @@
 #define MODBUSIP_MAXFRAME 200
 #define MODBUSIP_TIMEOUT   10
 
-class ModbusIP : public Modbus {
-    private:
-        byte _MBAP[7];
-    public:
-        ModbusIP();
-        void config(const char* ssid, const char* password);
-        void task();
+class ModbusIP: public Modbus {
+private:
+	byte _MBAP[7];
+public:
+	ModbusIP();
+	void config(const char* ssid, const char* password);
+	void configClient(const char* ssid, const char* password);
+	void task();
 };
 
 #endif //MODBUSIP_ESP8266_H
-
