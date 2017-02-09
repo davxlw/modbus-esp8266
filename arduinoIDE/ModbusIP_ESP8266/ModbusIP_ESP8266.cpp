27d0e1c71fc22abe66193c10d6f4b796a992e935
davxlw <davxlw@free.fr>
2017-02-09 13:37:19 +0100

    sendRequest() method declaration

--- arduinoIDE/ModbusIP_ESP8266/ModbusIP_ESP8266.cpp
+++ arduinoIDE/ModbusIP_ESP8266/ModbusIP_ESP8266.cpp
@@ -17,7 +17,6 @@ void ModbusIP::config(const char* ssid, const char* password) {
 
 void ModbusIP::configClient(const char* ssid, const char* password) {
 	WiFi.begin(ssid, password);
-	WiFiClient client;
 }
 
 void ModbusIP::task() {
--- arduinoIDE/ModbusIP_ESP8266/ModbusIP_ESP8266.h
+++ arduinoIDE/ModbusIP_ESP8266/ModbusIP_ESP8266.h
@@ -19,6 +19,7 @@ public:
 	ModbusIP();
 	void config(const char* ssid, const char* password);
 	void configClient(const char* ssid, const char* password);
+	void sendRequest(const char* host);
 	void task();
 };
 
